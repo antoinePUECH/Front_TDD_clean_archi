108584421c809ade580570a47ae6d2ec134b10a3
Administrator <admin@example.com>
2022-11-17 16:38:02 +0100

    fix update client

--- src/coreLogic/gateways/ClientGateway.ts
+++ src/coreLogic/gateways/ClientGateway.ts
@@ -2,6 +2,6 @@ import { Client } from '~/src/coreLogic/usecases/client-listing/client'
 
 export interface ClientGateway {
   listAll(): Promise<Array<Client>>
-  updateClient(client: any): Promise<Array<any>>
+  updateClient(client: Client): Promise<void>
   add(client: Client): Promise<void>
 }
