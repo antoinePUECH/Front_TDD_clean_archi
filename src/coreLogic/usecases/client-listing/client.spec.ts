c5f6a706f7132088b9e1814457a0a55910d1ea63
Antoine Puech <antoine.puech@edu.devinci.fr>
2022-11-17 12:12:48 +0100

    rename variables

--- src/coreLogic/usecases/client-listing/client.spec.ts
+++ src/coreLogic/usecases/client-listing/client.spec.ts
@@ -1,7 +1,7 @@
 import {InMemoryClientGateway} from '../../../adapters/secondary/InMemoryClientGateway';
 import { useClientStore } from '~/src/store/clients';
 import { createPinia, setActivePinia } from 'pinia'
-import { listProducts } from '~/src/coreLogic/usecases/client-listing/listClients';
+import { listClients } from '~/src/coreLogic/usecases/client-listing/listClients';
 import { Client } from '~/src/coreLogic/usecases/client-listing/client';
 
 describe('list client', () => {
@@ -14,29 +14,29 @@ describe('list client', () => {
   })
   describe('There is no available clients', () => {
     it('should list nothing', async () => {
-      await listProducts(clientGateway)
+      await listClients(clientGateway)
       expect(clientStore.all).toEqual([])
     })
   })
   describe('There is available clients', () => {
     it('should list all products', async () => {
-      const guitare: Client = { id: 'abc123', name: 'Vincent', email: 'xxxx@gmail.Com' }
-      const basse: Client = { id: 'def456', name: 'Antoine', email: 'akaxa@gmail.com' }
-      clientGateway.feedWith(guitare, basse)
-      await listProducts(clientGateway)
-      expect(clientStore.all).toEqual([guitare, basse])
+      const vincent: Client = { id: 'abc123', name: 'Vincent', email: 'xxxx@gmail.Com' }
+      const antoine: Client = { id: 'def456', name: 'Antoine', email: 'akaxa@gmail.com' }
+      clientGateway.feedWith(vincent, antoine)
+      await listClients(clientGateway)
+      expect(clientStore.all).toEqual([vincent, antoine])
     })
   })
   describe('Loading', () => {
     it('should be aware during loading', async () => {
-      const unsubscribe = clientStore.$subscribe((mutation: any, state: any) => {
+      const unsubscribe = clientStore.$subscribe((state: any) => {
         expect(state.isLoading).toBeTruthy()
         unsubscribe()
       })
-      await listProducts(clientGateway)
+      await listClients(clientGateway)
     })
     it('should finish loading when done', async () => {
-      await listProducts(clientGateway)
+      await listClients(clientGateway)
       expect(clientStore.isLoading).toBeFalsy()
     })
   })
--- src/coreLogic/usecases/client-listing/listClients.ts
+++ src/coreLogic/usecases/client-listing/listClients.ts
@@ -1,7 +1,7 @@
 import { ClientGateway } from '~/src/coreLogic/gateways/ClientGateway'
 import { useClientStore } from '~/src/store/clients'
 
-export const listProducts = async (clientGateway: ClientGateway) => {
+export const listClients = async (clientGateway: ClientGateway) => {
   const clientStore = useClientStore()
   clientStore.startListing()
   const products = await clientGateway.listAll()
